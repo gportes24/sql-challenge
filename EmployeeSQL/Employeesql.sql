6f7d252400dc5b0f2e0af2a604149ed5b89f43fe
gportes24 <gportes24@gmail.com>
2020-08-25 21:07:56 -0400

    final push, HW complete

--- EmployeeSQL/Employeesql.sql
+++ EmployeeSQL/Employeesql.sql
@@ -77,3 +77,64 @@ left join department
 on dept_manager.dept_no = department.dept_no
 left join employees
 on dept_manager.emp_no = employees.emp_no
+
+-- 4 List the department of each employee with the following information: employee number, last name, first name, and department name.
+
+SELECT dept_emp.emp_no,
+employees.last_name,
+employees.first_name,
+department.dept_name
+FROM dept_emp
+LEFT JOIN department
+ON dept_emp.dept_no = department.dept_no
+LEFT JOIN employees
+ON dept_emp.emp_no = employees.emp_no
+
+
+--5 List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+
+SELECT employees.first_name,
+employees.last_name,
+employees.sex
+FROM employees
+WHERE first_name = 'Hercules' AND last_name like 'B%'
+
+--6 List all employees in the Sales department, including their employee number, last name, first name, and department name.
+select * from department
+select * from employees
+select * from dept_emp
+
+SELECT dept_emp.emp_no,
+employees.last_name,
+employees.first_name,
+department.dept_name
+FROM dept_emp
+LEFT JOIN department
+ON dept_emp.dept_no = department.dept_no
+LEFT JOIN employees
+ON dept_emp.emp_no = employees.emp_no
+where department.dept_name ='Sales'
+
+
+-- 7 List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+
+SELECT dept_emp.emp_no,
+employees.last_name,
+employees.first_name,
+department.dept_name
+FROM dept_emp
+LEFT JOIN department
+ON dept_emp.dept_no = department.dept_no
+LEFT JOIN employees
+ON dept_emp.emp_no = employees.emp_no
+where department.dept_name ='Sales' OR dept_name= 'Development'
+
+-- 8 In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+
+select employees.last_name,
+count(employees.last_name)
+from employees
+group by last_name
+order by count(last_name) desc
+
